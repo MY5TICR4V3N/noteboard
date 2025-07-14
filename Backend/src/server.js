724a0b357a9b64bde29f9be72102cbdbea3a31d3
MY5TICR4V3N <alvissajith@gmail.com>
2025-07-15 00:18:13 +0530

    depoly

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,12 +4,18 @@ import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 const PORT=process.env.PORT || 5000;
 const app = express();
+const __dirname = path.resolve();
 
-app.use(cors({origin: 'http://localhost:5173'})); // Allow requests from the frontend
+if(process.env.NODE_ENV !== 'production') {
+
+  app.use(cors({origin: 'http://localhost:5173'})); // Allow requests from the frontend
+
+}
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(rateLimiter);
 
@@ -20,6 +26,15 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/notes",notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+  app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+});
+
+}
+
 
 connectDB().then(() => {
 app.listen(PORT, () => {
